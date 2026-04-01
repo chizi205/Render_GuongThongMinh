2d06c5a40c818d9e1a8a8b70912ab7b20d3cbe84
chizi205 <vin20464@gamil.com>
2026-04-01 11:12:56 +0700

    update zalooa.routes.js

--- src/routes/webhooks/zalooa.routes.js
+++ src/routes/webhooks/zalooa.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/zalo", (req,res)=>{
-    console.log("webhook zalo")
+router.post("/zalo", (req, res) => {
+  console.log("Zalo webhook:", req.body);
+  res.status(200).send("OK");
 });
 
 module.exports = router;
